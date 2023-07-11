0172d5c268bb60c3dba457aa2a077c4a40464107
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-07-11 16:19:05 -0600

    2

--- src/pages/users/userProfile.jsx
+++ src/pages/users/userProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import UserById from '../../components';
+import UserById from '../../components/userById';
 
 const userProfile = () => {
   return (
